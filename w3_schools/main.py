216cb7aa3321fe356b3944f828c8db0307dd20fe
ebenezerasabre <ebenezerasabre@gmail.com>
2024-09-04 14:30:51 -0400

    first commit

---
+++ w3_schools/main.py
@@ -0,0 +1,20 @@
+import numpy
+from scipy import stats
+
+
+
+speed = [99,86,87,88,111,86,103,87,94,78,77,85,86]
+
+x = numpy.mean(speed)
+
+x_median = numpy.median(speed)
+
+x_mode = stats.mode(speed)
+
+print(x)
+
+print(x_median)
+
+print(x_mode)
+
+
